9b75686c8b466e4562b1f35353fc2833b1a5aa43
BuNaC <cristi_bz09@yahoo.com>
2018-03-28 22:02:57 +0300

    Changes/improvements over the past month

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,2 @@
-var gulp = require("gulp"),
-    watch = require("gulp-watch");
-
-gulp.task("default", function() {
-    console.log("Hooray- You created a gulp task.");
-});
-gulp.task("html", function() {
-    console.log("Imagine somenting usefull being done to your HTML here.");  
-})
-gulp.task("styles", function() {
-    console.log("Imagine Sass or PostCSS tasks running here");
-});
-
-gulp.task("watch", function(){
-    watch("./app/index.html", function() {
-        gulp.start("html");
-    });  
-    watch("./app/assets/styles/**/*.css", function() {
-        gulp.start("styles");
-    });
-});
+require("./gulp/tasks/styles");
+require("./gulp/tasks/watch");
